0bdfa30cc37fffb9c3a0129a9a452bddb5ce422f
mathilde <math68n8@stud.kea.dk>
2022-02-06 17:49:03 +0100

    animations added, and new game works

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded", start);
 function start() {
     console.log("game started");
 
-    const buttons = document.querySelectorAll("#buttons button")
+    const buttons = document.querySelectorAll("#buttons button");
     buttons.forEach(button => button.addEventListener("click", getPlayersChoice));
 }
 
 
 let player_choice;
 let computer_choice;
+
 
 function getPlayersChoice() {
 
@@ -23,7 +24,7 @@ function getPlayersChoice() {
         player_choice = "scissors";
     }
 
-    console.log("set playerchoice = " + player_choice);
+    console.log("players choice: " + player_choice);
 
 
     makeComputerChoice();
@@ -35,64 +36,93 @@ function makeComputerChoice() {
     const comp_choice_Array = ["rock", "paper", "scissors"];
     //a random number between 0 and 2
     const random = Math.floor(Math.random() * 3);
-    console.log(random);
     // sets computer choice equal to one of choices from the array, using the random number
     computer_choice = comp_choice_Array[random];
 
-    console.log("computerchoice= " + computer_choice);
+    console.log("computers choice: " + computer_choice);
 
 
     showAnimations();
 }
 
 function showAnimations() {
+    document.querySelector("#player1").classList.add("shake");
+    document.querySelector("#player2").classList.add("shake");
     console.log("animationsShowing");
-    determineWinner();
+    document.querySelector("#player1").addEventListener("animationend",determineWinner);
+    
 }
 
 function determineWinner() {
-    console.log("determining the winner");
+    console.log("determining the winner...");
+
+    
+    document.querySelector("#player1").classList.remove("shake");
+    document.querySelector("#player2").classList.remove("shake");
+
+    document.querySelector("#player1").classList.add(player_choice);
+    document.querySelector("#player2").classList.add(computer_choice);
 
     //draw
     if (player_choice == computer_choice) {
-        console.log("its a draw");
+        console.log("its a DRAW with " + player_choice + " against " + computer_choice);
         showTie();
-    } else if(player_choice == "rock" & computer_choice =="scissors" || player_choice =="paper" & computer_choice == "rock" || player_choice=="scissors" & computer_choice=="paper"){
-       console.log("the player wins") 
+    } 
+    //win
+    else if(player_choice == "rock" & computer_choice =="scissors" || player_choice =="paper" & computer_choice == "rock" || player_choice=="scissors" & computer_choice=="paper"){
+       console.log("you WIN with " + player_choice + " against " + computer_choice);
         showWin();
-    }else{
-        console.log("you lose, comp wins");
+    }
+    //lose
+    else{
+       console.log("you LOSE with " + player_choice + " against " + computer_choice);
+        
         showLose();
     }
     
-    
-    
-    
-    /*else if (player_choice == "rock") {
-        if (computer_choice == "paper") {
-            showLose();
-            console.log("its a lose");
-        } else if (computer_choice == "scissors") {
-            showWin();
-            console.log("its a win");
 
-        }
-    }*/
 }
 
+
 function showWin() {
+    console.log("WINNER");
     document.querySelector("#win").classList.remove("hidden");
+    const buttons = document.querySelectorAll("#buttons button");
+    buttons.forEach(button => button.addEventListener("click", newGame));
 
 }
 function showLose() {
+    console.log("LOSER");
+
     document.querySelector("#lose").classList.remove("hidden");
+    const buttons = document.querySelectorAll("#buttons button");
+    buttons.forEach(button => button.addEventListener("click", newGame));
+
 }
 function showTie() {
-    console.log("there is no winner");
+    console.log("no winner");
     document.querySelector("#draw").classList.remove("hidden");
+    const buttons = document.querySelectorAll("#buttons button");
+    buttons.forEach(button => button.addEventListener("click", newGame));
+
 }
 
-function newGame() { }
+function newGame() { 
+    console.log("new game started");
+    document.querySelector("#lose").classList.add("hidden");
+    document.querySelector("#win").classList.add("hidden");
+    document.querySelector("#draw").classList.add("hidden");
+
+    document.querySelector("#player1").classList.remove("rock");
+    document.querySelector("#player1").classList.remove("paper");
+    document.querySelector("#player1").classList.remove("scissors");
+
+    document.querySelector("#player2").classList.remove("rock");
+    document.querySelector("#player2").classList.remove("paper");
+    document.querySelector("#player2").classList.remove("scissors");
+
+
+}
 
 
 
